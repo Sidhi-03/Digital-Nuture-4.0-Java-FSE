d9fa57f91d65d72b01d03ac6a37f3c8b184f62d5
Sidhi <113815852+Sidhi-03@users.noreply.github.com>
2025-06-20 11:27:01 +0530

    Delete Week 1/Design Patterns and Principles/Exercise 2: Implementing the Factory Method Pattern/ExcelDocumentFactory.java

--- Week 1/Design Patterns and Principles/Exercise 2: Implementing the Factory Method Pattern/ExcelDocumentFactory.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.text.Document;
-
-public class ExcelDocumentFactory extends DocumentFactory{
-    @Override
-    public ExcelDocument createDocument(){
-        return new ExcelDocument();
-    }
-}
